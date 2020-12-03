146dc3204998af8b8f2c12e265d57250abf9a80f
Chris Hill <chris.hill18@nhs.net>
2020-12-03 10:05:27 +0000

    Tidy up day 2 code.

--- src/main/kotlin/aoc2020/DayTwo.kt
+++ src/main/kotlin/aoc2020/DayTwo.kt
@@ -2,57 +2,56 @@ package aoc2020
 
 class DayTwo {
     fun partOne(input: String) {
-        val tests = splitInput(input) {
-            s -> Rule(s.split(":")[0]) to s.split(":")[1]
+        parseInputAndExecute(input) {
+            s -> Rule1(s.split(":")[0]) to s.split(":")[1]
         }
-
-        val validPasswords = tests.count { test ->
-            val rule = test.key;
-            rule.isValid(test.value)
-        }
-
-        println(validPasswords)
     }
 
     fun partTwo(input: String) {
-        val tests = splitInput(input) {
+        parseInputAndExecute(input) {
             s -> Rule2(s.split(":")[0].trim()) to s.split(":")[1].trim()
         }
-
-        val validPasswords = tests.count { test ->
-            val rule = test.key;
-            rule.isValid(test.value)
-        }
-
-        println(validPasswords)
     }
 
-    private fun <K> splitInput(input: String, transform: (String) -> Pair<K, String>): Map<K, String> {
+    private fun parseInputAndExecute(input: String, transform: (String) -> Pair<Rule, String>) {
+        val tests = splitInput(input) { s -> transform.invoke(s) }
+
+        println(countValidPasswords(tests))
+    }
+
+    private fun splitInput(input: String, transform: (String) -> Pair<Rule, String>): Map<Rule, String> {
         return input.split("\n")
             .filter { s-> s.isNotEmpty() }
             .associate { s -> transform.invoke(s) }
     }
-
-    private class Rule(spec: String) {
-        val min = spec.split(" ")[0].split("-")[0].toInt()
-        val max = spec.split(" ")[0].split("-")[1].toInt()
-        val letter = spec.split(" ")[1][0]
-
-        fun isValid(value: String): Boolean {
-            val letterCount = value.filter { c -> c == letter }.length
-
-            return letterCount in min..max;
+    private fun countValidPasswords(tests: Map<Rule, String>): Int {
+        return tests.count { test ->
+            val rule = test.key;
+            rule.isValid(test.value)
         }
     }
 
-    private class Rule2(spec: String) {
-        val posOne = spec.split(" ")[0].split("-")[0].toInt() - 1
-        val posTwo = spec.split(" ")[0].split("-")[1].toInt() - 1
+    private abstract class Rule(spec: String) {
+        val lower = spec.split(" ")[0].split("-")[0].toInt()
+        val upper = spec.split(" ")[0].split("-")[1].toInt()
         val letter = spec.split(" ")[1][0]
 
-        fun isValid(value: String): Boolean {
-            val valOne = value[posOne]
-            val valTwo = value[posTwo]
+        abstract fun isValid(value: String): Boolean
+    }
+
+    private class Rule1(spec: String) : Rule(spec) {
+        override fun isValid(value: String): Boolean {
+            val letterCount = value.filter { c -> c == letter }.length
+
+            return letterCount in lower..upper;
+        }
+    }
+
+    private class Rule2(spec: String) : Rule(spec) {
+        override fun isValid(value: String): Boolean {
+            val valOne = value[lower]
+            val valTwo = value[upper]
+
             return (valOne != valTwo) && (valOne == letter || valTwo == letter)
         }
     }
