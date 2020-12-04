71fd1939ff205de02d0b4bbcaa81ceb285a460a7
Chris Hill <chris.hill18@nhs.net>
2020-12-04 16:50:17 +0000

    Remove duplicated code from Day4.

--- src/main/kotlin/aoc2020/Day4.kt
+++ src/main/kotlin/aoc2020/Day4.kt
@@ -4,15 +4,7 @@ class Day4 {
     fun partOne(input: String) {
         val requiredFields = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
 
-        val passports = sanitiseInput(input)
-
-        val validPassportCount = passports
-            .map { passport ->
-                passport
-                    .trim()
-                    .split(" ")
-                    .associate { f -> f.split(":")[0] to f.split(":")[1] }
-            }
+        val validPassportCount = sanitiseInput(input)
             .count { fields -> requiredFields.all { f -> fields.containsKey(f) }}
 
         println(validPassportCount)
@@ -28,9 +20,30 @@ class Day4 {
             "ecl" to { s: String -> s.matches(Regex("amb|blu|brn|gry|grn|hzl|oth")) },
             "pid" to { s: String -> s.matches(Regex("[0-9]{9}")) })
 
-        val passports = sanitiseInput(input)
+        val validPassportsCount = sanitiseInput(input)
+            .filter { fields -> requiredFields.all { (name, _) -> fields.containsKey(name) }}
+            .count { fields -> fields.all { (name, value) -> requiredFields.getValue(name).invoke(value)}}
 
-        val validPassportsCount = passports
+        println(validPassportsCount)
+    }
+
+    private fun validateHeight(input: String): Boolean {
+        val (value, unit) = Regex("(\\d+)(\\w{2})").find(input)
+            ?.let {match -> match.groupValues[1] to match.groupValues[2]}
+            ?: "0" to "mm"
+
+        return when (unit) {
+            "cm" -> value.toInt() in 150..193
+            "in" -> value.toInt() in 59..76
+            else -> false
+        }
+    }
+
+    private fun sanitiseInput(input: String): List<Map<String, String>> {
+        return input
+            .replace("\n\n", "^")
+            .replace("\n", " ")
+            .split("^")
             .map { passport ->
                 passport
                     .trim()
@@ -38,30 +51,5 @@ class Day4 {
                     .associate { f -> f.split(":")[0] to f.split(":")[1] }
                     .filter { (name, _) -> name != "cid" }
             }
-            .filter { fields -> requiredFields.all { (name, _) -> fields.containsKey(name) }}
-            .count { fields -> fields.all { (name, value) -> requiredFields[name]!!.invoke(value)}}
-
-        println(validPassportsCount)
-    }
-
-    private fun validateHeight(input: String): Boolean {
-        val match = Regex("(\\d+)(\\w{2})").find(input)
-        if (match != null) {
-            val (value, unit) = match.destructured
-            return when (unit) {
-                "cm" -> value.toInt() in 150..193
-                "in" -> value.toInt() in 59..76
-                else -> false
-            }
-        }
-
-        return false
-    }
-
-    private fun sanitiseInput(input: String): List<String> {
-        return input
-            .replace("\n\n", "^")
-            .replace("\n", " ")
-            .split("^")
     }
 }
